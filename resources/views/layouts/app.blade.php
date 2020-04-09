5f8f9ac9f410a89506ebb51eefd864e3b16b7dbe
pabloveintitres <pabloveintitres@gmail.com>
2020-04-09 05:56:58 +0200

    0.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    InstaBalmes
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pr-3" id="divLogo"><img src="/svg/InstaBalmesLogo.svg" alt="" id="logo"></div>
+                    <div class="pl-3">InstaBalmes</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
