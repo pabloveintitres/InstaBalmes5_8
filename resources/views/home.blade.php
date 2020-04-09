249519f201854fbe3cc0502603ce97a771d86883
pabloveintitres <pabloveintitres@gmail.com>
2020-04-10 00:36:46 +0200

    0.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-9"></div>
+        <div class="col-3">
+            <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-19/s320x320/30589887_2010876322572326_834434668697747456_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_ohc=AzzTIOXMDZ4AX-l7ZmY&oh=8c8317fcb609865cf40c24074086d2c0&oe=5EBA5708" alt="" class="pb-5 rounded-circle" id="profileImage">
+        </div>
+        <div class="col-9">
+
+        </div>
     </div>
 </div>
 @endsection
