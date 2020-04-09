4edc17fae24b8cac36153565f3e4fe75a77c8954
pabloveintitres <pabloveintitres@gmail.com>
2020-04-10 01:43:03 +0200

    0.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-            <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-19/s320x320/30589887_2010876322572326_834434668697747456_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_ohc=AzzTIOXMDZ4AX-l7ZmY&oh=8c8317fcb609865cf40c24074086d2c0&oe=5EBA5708" alt="" class="pb-5 rounded-circle" id="profileImage">
+        <div class="col-3 pt-5 pl-5">
+            <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-19/s320x320/30589887_2010876322572326_834434668697747456_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_ohc=AzzTIOXMDZ4AX-l7ZmY&oh=8c8317fcb609865cf40c24074086d2c0&oe=5EBA5708" alt="" class="rounded-circle" id="profileImage">
         </div>
-        <div class="col-9">
-
+        <div class="col-9 pt-5 pl-5">
+            <div>
+                <h1>Visca La Puri</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5">153</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-3">Al voltant del Carrer Puríssima Concepció: el seus habitants, el seus espais, la seva història. #ViscaLaPuri #ElSolarDeLaPuri</div>
+            <div><strong><a href="https://www.instagram.com/visca_la_puri/">https://www.instagram.com/visca_la_puri/</a></strong></div>
         </div>
     </div>
 </div>
