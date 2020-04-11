0f86154b21030a8e4946c7ec9d5e9f95bf3780b6
pabloveintitres <pabloveintitres@gmail.com>
2020-04-11 18:24:48 +0200

    0.2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,23 +2,39 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="form-group row">
-                    <label for="caption" class="col-md-4 col-form-label">Post caption</label>
-                    <input id="caption"
-                           type="text" class="form-control @error('caption') is-invalid @enderror"
-                           caption="caption" value="{{ old('caption') }}"
-                           required autocomplete="caption" autofocus>
+        <form action="">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="form-group row">
+                        <label for="caption" class="col-md-4 col-form-label">Post caption</label>
+                        <input
+                            id="caption"
+                            type="text"
+                            class="form-control @error('caption') is-invalid @enderror"
+                            caption="caption" value="{{ old('caption') }}"
+                            required autocomplete="caption" autofocus>
 
-                    @error('caption')
-                    <span class="invalid-feedback" role="alert">
-                             <strong>{{ $message }}</strong>
-                         </span>
-                    @enderror
+                        @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <label for="image" class="col-md-4 col-form-label">Post image</label>
+                    <input type="file" name="image" id="image" class="form-control-file">
+
+                    @error('image')
+                    <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                    @enderror
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
